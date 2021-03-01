a0dde41f6160d349e18199548d8fca61d47fe37e
anton-dex <karpenko.ant@gmail.com>
2021-03-01 22:43:23 +0500

    Added few tests

--- GildedRoseTest.cs
+++ GildedRoseTest.cs
@@ -6,12 +6,68 @@ namespace csharpcore
     public class GildedRoseTest
     {
         [Fact]
-        public void foo()
+        public void Foo()
         {
-            IList<Item> Items = new List<Item> { new Item("foo", 0, 0) };
-            GildedRose app = new GildedRose(Items);
+            var Items = new List<Item> { new Item("foo", 0, 0) };
+            var app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal("foo", Items[0].Name);
         }
-    }
+
+		[Fact]
+		public void AgedBrieItemTest()
+		{
+			var Items = new List<Item> { new Item("Aged Brie", 1, 0) };
+			var app = new GildedRose(Items);
+			app.UpdateQuality();
+			Assert.Equal(0, Items[0].SellIn);
+			Assert.Equal(1, Items[0].Quality);
+
+			app.UpdateQuality();
+			Assert.Equal(-1, Items[0].SellIn);
+			Assert.Equal(3, Items[0].Quality);
+		}
+
+		[Fact]
+		public void SulfurasItemTest()
+		{
+			var Items = new List<Item> { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+			var app = new GildedRose(Items);
+			app.UpdateQuality();
+			Assert.Equal(0, Items[0].SellIn);
+			Assert.Equal(80, Items[0].Quality);
+
+			app.UpdateQuality();
+			Assert.Equal(0, Items[0].SellIn);
+			Assert.Equal(80, Items[0].Quality);
+		}
+
+		[Fact]
+		public void BackstageItemTest()
+		{
+			var Items = new List<Item> { new Item("Backstage passes to a TAFKAL80ETC concert", 6, 20) };
+			var app = new GildedRose(Items);
+			app.UpdateQuality();
+			Assert.Equal(5, Items[0].SellIn);
+			Assert.Equal(22, Items[0].Quality);
+
+			app.UpdateQuality();
+			Assert.Equal(4, Items[0].SellIn);
+			Assert.Equal(25, Items[0].Quality);
+		}
+
+		[Fact]
+		public void ElixirItemTest()
+		{
+			var Items = new List<Item> { new Item("Elixir of the Mongoose", 5, 7) };
+			var app = new GildedRose(Items);
+			app.UpdateQuality();
+			Assert.Equal(4, Items[0].SellIn);
+			Assert.Equal(6, Items[0].Quality);
+
+			app.UpdateQuality();
+			Assert.Equal(3, Items[0].SellIn);
+			Assert.Equal(5, Items[0].Quality);
+		}
+	}
 }
